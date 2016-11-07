bb74953a9ce2fc40de10a6f9f0878f4136b6bbe4
Matt Union <matt2union@utexas.edu>
2016-11-07 15:33:28 -0600

    added gitignore file and restuarant class

---
+++ EatMe/app/src/main/java/com/example/jason/liketmreal/Restaurant.java
@@ -0,0 +1,63 @@
+package com.example.jason.liketmreal;
+
+/**
+ * Created by mattunion on 11/7/16.
+ */
+
+public class Restaurant {
+    private String name;
+    private String address;
+    private String url;
+    private String phoneNumber;
+    private int rating;
+    private int price;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
