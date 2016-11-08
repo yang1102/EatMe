718f820ae4972cad55c24675646948554cf9c1d2
Matt Union <matt2union@utexas.edu>
2016-11-07 20:33:08 -0600

    added recycler view

---
+++ EatMe/app/src/main/java/com/example/jason/liketmreal/DynamicAdapter.java
@@ -0,0 +1,80 @@
+package com.example.jason.liketmreal;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.jason.liketmreal.R;
+import com.example.jason.liketmreal.Restaurant;
+
+import java.net.URL;
+import java.util.ArrayList;
+
+import static android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP;
+
+/**
+ * Created by mattunion on 10/16/16.
+ */
+
+public class DynamicAdapter extends RecyclerView.Adapter<DynamicAdapter.DynamicViewHolder> {
+    private ArrayList<Restaurant> resturants = null;//copy constructor or reference to arraylist in main_activity?
+
+    public class DynamicViewHolder extends RecyclerView.ViewHolder {
+        public TextView name;
+        //public TextView hiddenText;
+        public View v;
+
+        public DynamicViewHolder(View view) {
+            super(view);
+            name = (TextView) view.findViewById(R.id.restaurantName);
+            v = view;
+        }
+    }
+    public DynamicAdapter(ArrayList<Restaurant> restaurants) {
+        this.resturants = restaurants;
+    }
+
+    public Restaurant getItem(int position) {
+        return resturants.get(position);
+    }
+
+    @Override
+    public DynamicAdapter.DynamicViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.restaurant_list_cell, parent, false);
+
+        return new DynamicViewHolder(itemView);
+    }
+
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(DynamicAdapter.DynamicViewHolder viewHolder, int position) {
+        // Get the data model based on position
+        final Restaurant restaurant = resturants.get(position);
+
+        // Set item views based on your views and data model
+        TextView nameView = viewHolder.name;
+        nameView.setText(restaurant.getName());
+
+        viewHolder.v.setOnClickListener(new View.OnClickListener() {
+            //how to add url to bundle
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), restaurant.getName(), Toast.LENGTH_SHORT).show();//for testing purposes
+                //add intenet to run Restaurant homepage view activity
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return resturants.size();
+    }
+}
--- EatMe/app/src/main/java/com/example/jason/liketmreal/SearchResultsActivity.java
+++ EatMe/app/src/main/java/com/example/jason/liketmreal/SearchResultsActivity.java
@@ -2,12 +2,16 @@ package com.example.jason.liketmreal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class SearchResultsActivity extends AppCompatActivity {
     private ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
+    protected DynamicAdapter restaurantsAdapter = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +22,18 @@ public class SearchResultsActivity extends AppCompatActivity {
         restaurants = (ArrayList<Restaurant>) getIntent().getSerializableExtra("searchResults");//check null first??
 
         //test search results serialized okay
-        for(Restaurant restaurant: restaurants){
-            Toast.makeText(this, restaurant.getName(), Toast.LENGTH_SHORT).show();
-        }
+//        for(Restaurant restaurant: restaurants){
+//            Toast.makeText(this, restaurant.getName(), Toast.LENGTH_SHORT).show();
+//        }
+
+        // Lookup the recyclerview in activity layout
+        RecyclerView searchResultsRecycler = (RecyclerView) findViewById(R.id.searchResults);
+
+        restaurantsAdapter = new DynamicAdapter(restaurants);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        searchResultsRecycler.setLayoutManager(mLayoutManager);
+        //searchResultsRecycler.setItemAnimator(new DefaultItemAnimator());
+        searchResultsRecycler.setAdapter(restaurantsAdapter);
     }
 }
